850d4f0e57bd30c1c11b175d374e8da747bf3570
javiOlaya <jjosaez@gmail.com>
2019-10-29 16:32:23 +0100

    funcion codificar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// wri
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Ingrese el Mensaje: ");
+        String mensaje  = scanner.nextLine();
+
+        String codigo =CodificarCesar(mensaje);
+        String codigo1=codigo;
+        codigo = codigo.substring(codigo.length()-1, codigo.length()-1);
+        System.out.println(codigo);
+
+        //String frase=DecodificarCesar(cifra);
     }
+    public static String CodificarCesar(String mensaje){
+
+        String cifra = "";
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Dime el número de pasos: ");
+        int pasos = scanner1.nextInt();
+        String num  =Integer.toString(pasos);
+        char [] letras = mensaje.toCharArray();
+
+
+        for(int i=0;i<letras.length;i++)
+            cifra += (char)( ((int) letras[i])+pasos) ;
+            String codigo=cifra+num;
+        return codigo;
+    }
+
+
 }
